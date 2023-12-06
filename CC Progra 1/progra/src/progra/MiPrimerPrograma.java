bf196dc91eaaf299f7792a2b7624d213807cf22a
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2023-12-06 12:59:19 -0500

    Add files via upload

---
+++ CC Progra 1/progra/src/progra/MiPrimerPrograma.java
@@ -0,0 +1,24 @@
+package progra;
+
+public class MiPrimerPrograma {
+	
+	//Inicio proceso
+	public static void main(String[] args) {
+	
+		//Variables
+		String mensajeNombre, mensajeEdad, mensajeCiudad;
+		
+		mensajeNombre="Claudia";
+		mensajeEdad="20";
+		mensajeCiudad="Quito";
+		//operaciones
+		
+		//Resultados
+		System.out.println("Hola Mundo");//println significa empieza en una nueva linea
+		System.out.println("Me llamo Claudia \nVivo en Quito \nTengo 20 años");
+		System.out.println("Mi nombre es "+ nombre);
+		System.out.println("Tengo "+ edad);
+		System.out.println("Vivo en "+ ciudad);
+	}
+	//Fin proceso
+}
