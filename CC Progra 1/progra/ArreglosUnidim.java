371c520eff03ed1f0fb0e262b87a97fd81d4e2e1
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2024-01-31 12:41:17 -0500

    Update ArreglosUnidim.java
    Todo listo

--- CC Progra 1/progra/ArreglosUnidim.java
+++ CC Progra 1/progra/ArreglosUnidim.java
@@ -3,7 +3,9 @@
  el rango
  La segunda funcion deseo sumar los 2 arreglos por posicion
  La tercera concatenar los numeros de los dos arreglos
- La cuarta es determinar el minimo valor de los dos arreglos*/
+ La cuarta es determinar el minimo valor de los dos arreglos
+*/
+ 
 import java.util.Scanner;
 public class ArreglosUnidim {
 
@@ -11,16 +13,22 @@ public class ArreglosUnidim {
 		// TODO Auto-generated method stub
 		int tamArrUno = 0, tamArrDos = 0;
 		int tamComparable = 0;
+		int tamArrDosCorreguido =0, tamArrUnoCorreguido = 0;
 		Scanner ingreso = new Scanner(System.in);
+		
+        do{
+            System.out.println("Ingrese el tamaño del primer arreglo");
+		    tamArrUno = Integer.parseInt(ingreso.nextLine());
+            
+        }while(tamArrUno<0);
+		
+		do{
+		    System.out.println("Ingrese el tamaño del segundo arreglo");
+	    	tamArrDos = Integer.parseInt(ingreso.nextLine());
+		
+		}while(tamArrDos<0);
 		int[] arregloUno = new int[tamArrUno];
 		int[] arregloDos = new int[tamArrDos];
-		
-
-		System.out.println("Ingrese el tamaño del primer arreglo");
-		tamArrUno = Integer.parseInt(ingreso.nextLine());
-		
-		System.out.println("Ingrese el tamaño del segundo arreglo");
-		tamArrDos = Integer.parseInt(ingreso.nextLine());
 		
 		arregloUno = llenarArregloEnt(tamArrUno);
 		arregloDos = llenarArregloEnt(tamArrDos);
@@ -29,18 +37,61 @@ public class ArreglosUnidim {
 		imprimirArregloEnt(arregloUno);
 		System.out.print(" y: ");
 		imprimirArregloEnt(arregloDos);
+		imprimirTodo(tamArrUno, tamArrDos, arregloUno, arregloDos);
 		
-		tamComparable = determinarTamIgual(tamArrUno, tamArrDos);
+		
+	}
+	/**
+	 *Esta funcion es para imprimir todo lo que me pide
+	 * @param tamArrUno tamaño del primer arreglo
+	 * @param tamArrDos tamaño del segundo arreglo
+	 * @param arregloUno arreglo de enteros
+	 * @param arregloDos segundo arreglo de enteros
+	 */
+	 public static void imprimirTodo(int tamArrUno, int tamArrDos, int[] arregloUno, int[] arregloDos){
+        int tamComparable = determinarTamIgual(tamArrUno, tamArrDos);
 		
 		if(tamComparable == 1) {
-			tamArrDos = tamArrDos + (tamArrUno-tamArrDos);
+			int tamArrDosCorreguido1 = tamArrDos + (tamArrUno-tamArrDos);
+			int[] arregloDosCorreguido = new int[tamArrDosCorreguido1];
+			arregloDosCorreguido = aumentarElemArreEnt(arregloDos, tamArrDosCorreguido1);
+			
+		    System.out.print("Ahora los arreglo son: ");
+    		imprimirArregloEnt(arregloUno);
+    		System.out.print(" y: ");
+    		imprimirArregloEnt(arregloDosCorreguido);
+    		
+			System.out.println("La suma de los dos arreglo es ");
+		    imprimirArregloEnt(determinarSumaArreglos(arregloUno, arregloDosCorreguido));
+		    imprimirConcatencacionArreEnt(arregloUno, arregloDosCorreguido);
+		    
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloUno));
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloDosCorreguido));
 		}else if(tamComparable == 2) {
-			tamArrUno = tamArrUno + (tamArrDos-tamArrUno);
+			int tamArrUnoCorreguido1 = tamArrUno + (tamArrDos-tamArrUno);
+			int[] arregloUnoCorreguido = new int[tamArrUnoCorreguido1];
+			arregloUnoCorreguido = aumentarElemArreEnt(arregloUno, tamArrUnoCorreguido1);
+			
+		    System.out.print("Ahora los arreglo son: ");
+    		imprimirArregloEnt(arregloUnoCorreguido);
+    		System.out.print(" y: ");
+    		imprimirArregloEnt(arregloDos);
+			
+			System.out.println("La suma de los dos arreglo es ");
+		    imprimirArregloEnt(determinarSumaArreglos(arregloUnoCorreguido, arregloDos));
+		    imprimirConcatencacionArreEnt(arregloUnoCorreguido, arregloDos);
+		    
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloUnoCorreguido));
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloDos));
+		}else if(tamComparable == 3){
+		    System.out.println("La suma de los dos arreglo es ");
+		    imprimirArregloEnt(determinarSumaArreglos(arregloUno, arregloDos));
+		    imprimirConcatencacionArreEnt(arregloUno, arregloDos);
+		    
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloUno));
+		    System.out.println("El menor numero del primer arreglo es: " + determinarNumMen(arregloDos));
 		}
-		
-		System.out.println("La suma de los dos arreglo es ");
-		determinarSumaArreglos(arregloUno, arregloDos);
-	}
+	 }
 	/**
 	 * Esta funcion me sumara los valores de los arreglos 
 	 * @param arregloUno
@@ -80,12 +131,12 @@ public class ArreglosUnidim {
 		}
 		return arregloNuevo;
 	}
-/**
- * Esta funcion determinara si los arreglos tiene el mismo tamaño y cual es mayor 
- * @param tamArrUno es el tamaño del primer arreglo
- * @param tamArrDos es el tamaño del segundo arreglo
- * @return entero que depende cual es el arreglo de mayor tamaño
- */
+    /**
+     * Esta funcion determinara si los arreglos tiene el mismo tamaño y cual es mayor 
+     * @param tamArrUno es el tamaño del primer arreglo
+     * @param tamArrDos es el tamaño del segundo arreglo
+     * @return entero que depende cual es el arreglo de mayor tamaño
+     */
 	public static int determinarTamIgual(int tamArrUno, int tamArrDos) {
 		// TODO Auto-generated method stub
 		if(tamArrUno>tamArrDos)
@@ -94,4 +145,48 @@ public class ArreglosUnidim {
 			return 2;
 		return 3;
 	}
+	/**
+	 * Esta funcion es para aumentar elementos de un arreglo
+	 * @param arreglo sera el arreglo de enteros que queremos aumentar
+	 * @param nuevoSize es un entero que es el valor del tamaño para el arreglo
+	 * @return un arreglo de enteros
+	 * */
+	 public static int[] aumentarElemArreEnt(int[] arreglo, int nuevoSize){
+	     int[] arregloResult = new int[nuevoSize];
+	     for(int cont = 0; cont < arreglo.length; cont++){
+	         arregloResult[cont] = arreglo[cont];
+	     }
+	     for(int contDos = arreglo.length; contDos < nuevoSize; contDos++){
+	         arregloResult[contDos] =(int)(Math.random()*100);
+	     }
+	     return arregloResult;
+	 }
+	 
+	 /**
+	  * Esta funcion es para imprimir la concatenacion de los elementos de dos arreglo 
+	  * @param arregloUno
+	  * @param arregloDos
+	  * */
+	  public static void imprimirConcatencacionArreEnt(int[] arregloUno, int[] arregloDos){
+	      System.out.print("La concatencacion es {");
+	      for(int i=0; i<arregloUno.length; i++){
+	          int numeroUno = arregloUno[i];
+	          System.out.print( "" +  numeroUno + "" + arregloDos[i] + " ,");
+	      }
+	      System.out.println("}");
+	  }
+	  
+	  /**
+	   * Esta funcion es para determinar el menor elemento de un arreglo
+	   * @param arreglo es un arreglo de enteros
+	   * @return numMen es un numero entero 
+	   * */
+	   public static int determinarNumMen(int [] arreglo){
+	       int numMen = arreglo[0];
+	       for(int cont = 0; cont<arreglo.length; cont++){
+	           if(numMen > arreglo[cont])
+	            numMen = arreglo[cont];
+	       }
+	       return numMen;
+	   }
 }
