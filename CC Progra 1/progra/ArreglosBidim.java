7f720d1c17093d59a540dee74f9f1439b4acded6
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2024-02-06 22:00:55 -0500

    Create ArreglosBidim.java

---
+++ CC Progra 1/progra/ArreglosBidim.java
@@ -0,0 +1,139 @@
+/*
+ * filla-----
+ * 
+ * Traer un metodo que imprima la metriz de tipo entero
+ * (con 2 for)
+ * 
+ * Traer un metodo
+ * que haga el llenado de la matriz por columnas
+ * 
+ * Traer la suma de valores de la fila
+ * en un nuevo arreglo
+ * 
+ * Traer la suma de columnas
+ * 
+ * Traer en una matriz cuadrada, sumar la traza (diagonal)
+ * */
+
+public class ArreglosBidim
+{
+	public static void main(String[] args) {
+		int[][] matriz = new int[8][8];
+		int numFilas = matriz.length;
+	    int numColumnas = matriz[0].length;
+	    
+		matriz = llenarMatrizDobleRand(numFilas, numColumnas);
+		mostrarMatrizEnt(matriz);
+		
+		int[] arregloColum = llenarArregloColumEnt(matriz);
+		int[] arregloFila = llenarArregloFilaEnt(matriz);
+		
+		System.out.print("La suma de las filas es: ");
+		imprimirMatriz(arregloFila);
+		System.out.print("La suma de las columnas es: ");
+		imprimirMatriz(arregloColum);
+		
+		int traza = calcularTraza(matriz);
+        System.out.println("La traza de la matriz es: " + traza);
+
+	}
+	/**
+	 * Esta funcion es para rellenar una matriz doble con numeros randoms
+	 * @author Claudia Coello
+	 * @param matriz es una matriz de enteros
+	 * @return una matriz de numeros enteros
+	 * */
+	public static int[][] llenarMatrizDobleRand (int numFilas, int numColumnas){
+	    int[][] matrizLlena = new int[numFilas][numColumnas];
+	    for(int i=0; i<numFilas; i++){
+	        for(int j=0; j<numColumnas; j++)
+	            matrizLlena[i][j] = (int)(((Math.random())*9));
+	    }
+	    return matrizLlena;
+	}
+	
+	/**
+	 * Esta funcion es para imprimir una matriz de enteros
+	 * @param matriz es una matriz de enteros
+	 * */
+	public static void mostrarMatrizEnt (int[][] matriz){
+	     
+	    int numFilas = matriz.length;
+	    int numColumnas = matriz[0].length;
+
+	    for(int i=0; i<numFilas; i++){
+	        System.out.print("{");
+	        for(int j=0; j<numColumnas; j++){
+	            System.out.print(matriz[i][j] + " ,");
+	            
+	        }
+	        System.out.print("}");
+	        System.out.println();
+	    }
+	    
+	 }
+	 
+	/**
+	 * Esta funcion es para llenar un arreglo con la suma de los valores de una fila
+	 * @author Claudia Coello
+	 * @param matriz es una matriz de enteros
+	 * @return un arreglo de enteros
+	 * */
+	public static int[] llenarArregloFilaEnt (int[][] matriz){
+	      int[] sumaFilas = new int[matriz.length];
+	      for(int i=0; i<matriz.length; i++){
+	          
+	          for(int j=0; j<matriz[0].length; j++){
+	              
+	          sumaFilas[i] += matriz[i][j]; 
+	        }
+	      }
+	      return sumaFilas;
+	  }
+	  
+	/**
+	 * Esta funcion es para llenar un arreglo con la suma de los valores de una fila
+	 * @author Claudia Coello
+	 * @param matriz es una matriz de enteros
+	 * @return un arreglo de enteros
+	 * */
+	public static int[] llenarArregloColumEnt (int[][] matriz){
+	      int[] sumaColum = new int[matriz.length];
+	      for(int i=0; i<matriz.length; i++){
+	          
+	          for(int j=0; j<matriz[0].length; j++){
+	              
+	          sumaColum[j] += matriz[i][j]; 
+	        }
+	      }
+	      return sumaColum;
+	  }
+	  
+	/**
+	 * Esta funcion es para imprimir un arreglo
+	 * @author Claudia Coello
+	 * @param arreglo es un arreglo de enteros
+	 * */
+	 public static void imprimirMatriz (int[] matriz) {
+	     System.out.print("{");
+		for(int i=0; i<matriz.length; i++){
+		    System.out.print(matriz[i] + " ,");
+		}
+		System.out.println("}");
+		
+	 }
+	/**
+     * Esta función calcula la traza (suma de los elementos en la diagonal) de una matriz cuadrada
+     * @param matriz es una matriz cuadrada de enteros
+     * @return la suma de los elementos en la diagonal de la matriz
+     */
+    public static int calcularTraza(int[][] matriz) {
+        int traza = 0;
+        
+        for (int i = 0; i < matriz.length; i++) {
+            traza += matriz[i][i];
+        }
+        return traza;
+    }
+
+}
